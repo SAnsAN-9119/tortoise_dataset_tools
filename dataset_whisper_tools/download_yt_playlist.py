7b72da56e53caea16e09bd9d9b62df22befb5f2a
ubuntu <q@q.q>
2024-04-22 14:14:52 +0500

    add folder with playlist name

--- dataset_whisper_tools/download_yt_playlist.py
+++ dataset_whisper_tools/download_yt_playlist.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import platform
+from pathlib import Path
 
 def play_sound():
     if platform.system() == 'Windows':
@@ -9,7 +10,7 @@ def play_sound():
     else:  # Linux
         os.system('aplay /usr/share/sounds/gnome/default/alerts/drip.ogg')
 
-def main(file_path, output_dir):
+def main(file_path, output_dir, save_by_playlist=False):
     '''
     This script is used to download youtube playlists, easily and simply for datasets.
     Follow the comments below.
@@ -29,11 +30,16 @@ def main(file_path, output_dir):
             if playlist_url:  # If the line is not empty
                 print(f'Downloading playlist: {playlist_url}')
 
+                if save_by_playlist:
+                    output_template = os.path.join(output_dir, '%(playlist_title)s/%(title)s.%(ext)s')
+                else:
+                    output_template = os.path.join(output_dir, '%(title)s.%(ext)s')
+
                 # Define the yt-dlp command
                 yt_dlp_cmd = [
                     'yt-dlp',
                     '-i',  # Ignore errors
-                    '-o', os.path.join(output_dir, '%(title)s.%(ext)s'),  # Output template
+                    '-o', output_template,  # Output template
                     '--no-part',  # Do not use .part files
                     '-f', 'bestaudio',  # Select the best audio quality
                     '--extract-audio',  # Only extract audio, not video
@@ -53,7 +59,9 @@ def main(file_path, output_dir):
 
 if __name__ == "__main__":
     # Path to the text file containing YouTube playlist URLs
-    file_path = '../../../audio/dataset_2_Ru/youtube_download_links_2.txt'
+    file_path = '../../../audio/dataset_3_Ru/youtube_download_links_3.txt'
     # Prompt for the output directory
-    output_dir = '../../modules/tortoise_dataset_tools/dataset_whisper_tools/dataset_2_Ru'
-    main(file_path, output_dir)
+    output_dir = '../../../audio/dataset_3_Ru/'
+    # Set save_by_playlist to True to create subfolders for each playlist
+    save_by_playlist = True
+    main(file_path, output_dir, save_by_playlist)
