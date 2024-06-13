b90bb32ac7779357bb205d4ae04c3d2af811fa17
SAnsAN <117441811+SAnsAN-9119@users.noreply.github.com>
2024-06-13 11:45:28 +0500

    convert to any format

--- audio_conversion_tools/convert_to_mp3.py
+++ audio_conversion_tools/convert_to_mp3.py
@@ -5,7 +5,8 @@ from tkinter import filedialog
 import multiprocessing
 from pydub import AudioSegment
 
-def convert_audio_to_mp3(source_path, mp3_path):
+
+def convert_audio(source_path, output_path, output_format):
     try:
         import time
         start = time.time()
@@ -13,22 +14,27 @@ def convert_audio_to_mp3(source_path, mp3_path):
         end = time.time()
         total_elapsed_time = end - start
         print(total_elapsed_time)
-        audio.export(mp3_path, format="mp3", bitrate="320k", parameters=["-ar", "22050"])
-        print(f"Converted {source_path} to {mp3_path}")
+        if output_format in ["mp3", "aac"]:
+            audio.export(output_path, format=output_format, bitrate="320k", parameters=["-ar", "22050"])
+        else:
+            audio.export(output_path, format=output_format)
+        print(f"Converted {source_path} to {output_path}")
     except Exception as e:
         print(f"Error converting {source_path}: {e}")
 
-def process_file(args):
-    source_path, converted_folder = args
-    file_name = os.path.basename(source_path)
-    mp3_path = os.path.join(converted_folder, file_name.rsplit('.', 1)[0] + ".mp3")
-    if file_name.lower().endswith(".mp3"):
-        shutil.copy2(source_path, mp3_path)
-        print(f"Copied {source_path} to {mp3_path}")
-    else:
-        convert_audio_to_mp3(source_path, mp3_path)
 
-def process_folder(folder_path, num_processes):
+def process_file(args):
+    source_path, converted_folder, output_format = args
+    file_name = os.path.basename(source_path)
+    output_path = os.path.join(converted_folder, file_name.rsplit('.', 1)[0] + f".{output_format}")
+    if file_name.lower().endswith(f".{output_format}"):
+        shutil.copy2(source_path, output_path)
+        print(f"Copied {source_path} to {output_path}")
+    else:
+        convert_audio(source_path, output_path, output_format)
+
+
+def process_folder(folder_path, output_format, num_processes):
     converted_folder = os.path.join(folder_path, "converted")
     if not os.path.exists(converted_folder):
         os.makedirs(converted_folder)
@@ -37,9 +43,10 @@ def process_folder(folder_path, num_processes):
     files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.split('.')[-1] in audio_extensions]
     
     pool = multiprocessing.Pool(processes=num_processes)
-    pool.map(process_file, [(file, converted_folder) for file in files])
+    pool.map(process_file, [(file, converted_folder, output_format) for file in files])
     pool.close()
     pool.join()
+
 
 def select_folder():
     root = tk.Tk()
@@ -47,10 +54,12 @@ def select_folder():
     folder_selected = filedialog.askdirectory(title="Select Folder")
     return folder_selected
 
+
 if __name__ == "__main__":
     folder_path = select_folder()
     if folder_path:
+        output_format = 'wav'   #'mp3'
         num_processes = multiprocessing.cpu_count()  # Use the number of CPU cores
-        process_folder(folder_path, num_processes)
+        process_folder(folder_path, output_format, num_processes)
     else:
         print("No folder selected. Exiting...")
